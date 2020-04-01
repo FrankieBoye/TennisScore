c62cfbc094f1f3504248463137a05af6be75e1bd
FrankieBoye <frankie4@btinternet.com>
2020-04-01 21:13:10 +0100

    refactored and added tests and code for advantage and wins

--- spec/tennisSpec.js
+++ spec/tennisSpec.js
@@ -1,12 +1,10 @@
 describe('TennisGame', function() {
 
-  var tennisGame;
-
   beforeEach(function(){
     tennisGame = new TennisGame();
   })
 
-  it('score starts at 0-0', function() {
+  it('score starts at love-love', function() {
     expect(tennisGame.currentScore()).toEqual('love-love')
   })
 
@@ -27,7 +25,68 @@ describe('TennisGame', function() {
     tennisGame.scorePointPlayer2()
     tennisGame.scorePointPlayer2()
     tennisGame.scorePointPlayer2()
-    expect(tennisGame.currentScore()).toEqual('Duece')
+    expect(tennisGame.currentScore()).toEqual('Deuce')
   })
-  
+
+  it('will show Advantage player1 if player 1 scores following Deuce', function() {
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer1()
+    expect(tennisGame.currentScore()).toEqual('Advantage player 1')
+  })
+
+  it('will show Advantage player2 if player 2 scores following Deuce', function() {
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer2()
+    expect(tennisGame.currentScore()).toEqual('Advantage player 2')
+  })
+
+  it('will revert to Duece if opposing player scores following Advantage', function() {
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer2()
+    expect(tennisGame.currentScore()).toEqual('Deuce')
+  })
+
+  it('will show Advantage player1 if player 1 scores following Deuce', function() {
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer1()
+    expect(tennisGame.currentScore()).toEqual('Advantage player 1')
+  })
+
+  it('will show player 1 has won if score is 50 and player 2 is 30 or less', function() {
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer1()
+    tennisGame.scorePointPlayer1()
+    expect(tennisGame.currentScore()).toEqual('Player 1 wins')
+  })
+
+  it('will show player 2 has won if score is 50 and player 1 is 30 or less', function() {
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer2()
+    tennisGame.scorePointPlayer2()
+    expect(tennisGame.currentScore()).toEqual('Player 2 wins')
+  })
+
 })
--- src/tennis.js
+++ src/tennis.js
@@ -5,7 +5,7 @@ function TennisGame(p1,p2){
   var player1score = 0
   var player2score = 0
 
-  TennisGame.prototype.currentScore = function() {
+  TennisGame.prototype.calculateScore = function() {
     if(this.player1 == 0) player1score = "love"
     if(this.player1 == 1) player1score = 15
     if(this.player1 == 2) player1score = 30
@@ -15,10 +15,21 @@ function TennisGame(p1,p2){
     if(this.player2 == 1) player2score = 15
     if(this.player2 == 2) player2score = 30
     if(this.player2 == 3) player2score = 40
+  }
 
-    if(this.player1 == 3 && this.player2 == 3) return "Duece"
+  TennisGame.prototype.currentScore = function() {
+    this.calculateScore()
+    var score = player1score + "-" + player2score
 
-    return player1score + "-" + player2score
+    if(this.player1 == 3 && this.player2 == 3) score = "Deuce"
+    if(this.player1 == 4 && this.player2 == 3) score = "Advantage player 1"
+    if(this.player1 == 3 && this.player2 == 4) score = "Advantage player 2"
+
+    if(this.player1 == 4 && this.player2 == 4) score = "Deuce"
+    if(this.player1 == 4 && this.player2 <= 2) score = "Player 1 wins"
+    if(this.player2 == 4 && this.player1 <= 2) score = "Player 2 wins"
+
+    return score
   }
 
   TennisGame.prototype.scorePointPlayer1 = function() {
